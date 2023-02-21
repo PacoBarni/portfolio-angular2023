7de165d720f886eeabd886e60da3d141911ab424
PacoBarni <paco12cv@hotmail.com>
2023-02-21 14:35:20 +0100

    commit

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -5,20 +5,30 @@ import { InfoPagina } from '../interfases/info-pagina.interfase';
 @Injectable({
   providedIn: 'root'
 })
-export class InfoPaginaService {    
+export class InfoPaginaService {   
   info: InfoPagina = {};
   cargada = false;
-
-
+  equipo:any = [];
   constructor( private http:HttpClient) {
-    // console.log('Servicio de infoPagina listo');
-  
-  this.http.get('assets/data/data-pagina.json')
-  .subscribe( (resp) =>{
-    this.cargada = true;
-    this.info = resp; // provar resp. I veurem les propietats JSON
-    console.log(resp);
-  });
+     this.cargarInfo();
+     this.cargarEquipo();
   }
 
+  private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+    this.cargada = true;
+    this.info = resp;
+    console.log(resp);
+});
+
+  }
+
+  private cargarEquipo() { //Donde revisamos la info de Firebase
+  this.http.get('https://db-storage-web-angular-default-rtdb.europe-west1.firebasedatabase.app/equip.json')
+  .subscribe( resp => {
+  this.equipo = resp;
+  console.log(resp);
+  });
+}
 }
